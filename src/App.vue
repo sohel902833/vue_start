01c635e266c66fd500397c3cd6e9881276561de5
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-01-29 15:07:41 +0600

    printing state to the ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>Hello {{ name }}</div>
+  <h3>{{ roll }}</h3>
+  <!-- using directive -->
+  <div v-text="company"></div>
+  <!-- javascript can use under mastas -->
+  <div>{{ `${5 + 7}` }}</div>
+  <!-- render html into dom -->
+  <div v-html="html"></div>
 </template>
 
 <script>
@@ -8,6 +15,9 @@ export default {
   data() {
     return {
       name: "Md Sohrab Hossain Sohel",
+      roll: "902833",
+      company: "Zaagsys",
+      html: "<h3 onclick='alert('You Hacked')'>Bolder Text</h3>",
     };
   },
 };
