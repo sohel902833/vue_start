ce97802a2575f8cf260a359ac0c7b37cb056817f
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-01-29 21:14:05 +0600

    created simple counter app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,62 @@
 <template>
-  <h1>Number {{ num }}</h1>
-  <h2 v-if="num === 0">The Number Is Zero</h2>
-  <h2 v-else-if="num < 0">The Number Is Negative</h2>
-  <h2 v-else-if="num > 0">The Number Is Positive</h2>
-  <h2 v-else>The Number Is Not Zero</h2>
-  <template v-if="display">
-    <h2>Md Sorab Hossain</h2>
-    <h3>Sohel</h3>
-  </template>
-  <h2 v-show="show">Using v-show</h2>
+  <div v-if="show" class="user_list">
+    <div v-for="(user, index) in data" class="user_item" :key="index">
+      <h2>Name :{{ user?.name }}</h2>
+      <h3>Roll:{{ user?.roll }}</h3>
+      <h3>Email:{{ index }}</h3>
+    </div>
+  </div>
+
+  <!-- object iterating   -->
+  <div v-for="(value, key, index) in myInfo" :key="value">
+    <h1>{{ value }} : {{ key }}, {{ index }}</h1>
+  </div>
+
+  <h1>Here Is Counter App</h1>
+  <h2>{{ counter }}</h2>
+  <div>
+    <button @click="add()">Increment By 1</button>
+    <button @click="add(5)">Increment By 5</button>
+    <button @click="sub()">Decrement By 1</button>
+    <button @click="sub(5)">Decrement By 5</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   data: () => {
+    const data = [...new Array(40)].map((it, i) => ({
+      name: "Sohel " + i,
+      roll: 902833 + i,
+    }));
     return {
-      num: 0,
-      display: true,
+      data: data,
       show: false,
+      myInfo: {
+        name: "Sohrab",
+        lastName: "Hossain",
+        phone: "234234",
+        roll: "902833",
+      },
+      counter: 1,
     };
+  },
+  methods: {
+    add(value = 1) {
+      this.counter += value;
+    },
+    sub(value = 1) {
+      this.counter -= value;
+    },
   },
 };
 </script>
+
+<style>
+.user_list {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
