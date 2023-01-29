8322473b8ad44c3b2899e33a8abda372a7a54ac3
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-01-29 14:59:20 +0600

    hello

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello {{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Md Sohrab Hossain Sohel",
+    };
+  },
+};
+</script>
